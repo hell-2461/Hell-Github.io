e490179a01fd20d2888d179fe24b8810535be58f
Tanner Godfrey <105381113+hell-2461@users.noreply.github.com>
2023-09-27 17:59:44 +0100

    Add files via upload

---
+++ myjsfile.js
@@ -0,0 +1,27 @@
+
+// Toggle grid padding
+function myFunction() {
+  var x = document.getElementById("myGrid");
+  if (x.className === "w3-row") {
+    x.className = "w3-row-padding";
+  } else { 
+    x.className = x.className.replace("w3-row-padding", "w3-row");
+  }
+}
+
+// Open and close sidebar
+function w3_open() {
+  document.getElementById("mySidebar").style.width = "100%";
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+
+
+
+
+body,h1 ;font-family; "Montserrat", sans-serif;
+ {margin-bottom; -7}
+;w3-row-padding ;pmargin-bottom; 12;
